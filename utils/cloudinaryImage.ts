72e4cbdc7061286eabaabf2a31cdb76afc4ff18f
Oskar Gregurek <opgregurek@gmail.com>
2023-12-17 17:25:52 +1000

    ensure width is an optional param

--- utils/cloudinaryImage.ts
+++ utils/cloudinaryImage.ts
@@ -1,6 +1,6 @@
 export default function cloudinaryImage(src: string, width?: number, quality?: number) {
     const params = ['f_auto', 'c_limit', `${width ? 'w_' + width : ''}`, `q_${quality || 'auto'}`];
-    return `https://res.cloudinary.com/dwet4ad3d/image/upload/${params.join(
+    return `https://res.cloudinary.com/dwet4ad3d/image/upload/${params.filter(x => !!x).join(
         ','
     )}/${src}`
 }

adae740613a3c39b52a44905bfc3f5420ed47895
Oskar Gregurek <opgregurek@gmail.com>
2023-12-18 10:45:47 +1000

    fix contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ import {Link} from "@chakra-ui/next-js";
 
 export default function Contact() {
     return (
-        <Box w="100%">
+        <Box w="100%" maxWidth="1440px" marginLeft="auto" marginRight="auto">
             <VStack alignItems="flex-start" gap={10}>
                 <SimpleGrid columns={2}>
                     <Text fontSize="80px" fontWeight="700" lineHeight="74px" textAlign="center">Get in touch</Text>
@@ -23,23 +23,23 @@ export default function Contact() {
                     </VStack>
                 </SimpleGrid>
                 <VStack alignItems="flex-start" gap={10}>
-                    <CloudinaryImage cloudinaryImageName="contact/spiral" alt="Spiral" width={67} height={67} />
+                    <CloudinaryImage cloudinaryImageName="contact/spiral" alt="Spiral" pixelWidth={67} pixelHeight={67} />
                     <Text color="text.lightGrey" w="365px">
                         We have learned so much, there still remains much to learn. We are not going in circles, we are going upwards. The path is a spiral.<br /><br />
                         Herman Hesse
                     </Text>
                     <HStack gap={3}>
                         <a href="https://instagram.com/jojobanks.jpg">
-                            <CloudinaryImage cloudinaryImageName="contact/instagram" alt="Instagram" width={37} height={37} />
+                            <CloudinaryImage cloudinaryImageName="contact/instagram" alt="Instagram" pixelWidth={37} pixelHeight={37} />
                         </a>
                         <a href="https://www.linkedin.com/in/josephinenguyen1/">
-                            <CloudinaryImage cloudinaryImageName="contact/linkedin" alt="LinkedIn" width={37} height={37} />
+                            <CloudinaryImage cloudinaryImageName="contact/linkedin" alt="LinkedIn" pixelWidth={37} pixelHeight={37} />
                         </a>
                         <a href="https://dribbble.com/jojobankss">
-                            <CloudinaryImage cloudinaryImageName="contact/dribbble" alt="Dribbble" width={37} height={37} />
+                            <CloudinaryImage cloudinaryImageName="contact/dribbble" alt="Dribbble" pixelWidth={37} pixelHeight={37} />
                         </a>
                         <a href="https://www.behance.net/josephinenguyen">
-                            <CloudinaryImage cloudinaryImageName="contact/behance" alt="Behance" width={37} height={37} />
+                            <CloudinaryImage cloudinaryImageName="contact/behance" alt="Behance" pixelWidth={37} pixelHeight={37} />
                         </a>
                     </HStack>
                 </VStack>

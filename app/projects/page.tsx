3e62b12ee8e16dc75225b36a346536e7e6cc72a3
Oskar Gregurek <opgregurek@gmail.com>
2023-12-20 02:54:41 +1000

    update project page linking

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 'use client'
-
-import { HStack, Text, Box } from "@chakra-ui/react";
+import {Text, Box, SimpleGrid} from "@chakra-ui/react";
 import {Link} from "@chakra-ui/next-js";
+import {Fragment} from "react";
 
 
 interface ProjectItem {
@@ -26,28 +26,33 @@ export default function Projects() {
     return (
 
         <Box w = "100%">
-            <>
+            <SimpleGrid templateColumns="auto 1fr" gap={[4, 4, 8, 16]}>
                 {ProjectItems.map((item, key) => {
                     return (
-                        <HStack marginBottom="1%" flex={1} key={key} alignItems="center">
-                            <Text flex={1} fontSize={["10px", "10px", "10px", "16px"]} fontWeight="400" color="text.lightGrey" >{ item.label } <br></br>{ item.year }</Text>
+                        <Fragment
+                            key={[item.label, item.year, item.href]
+                                .map(x => x.toLowerCase().replace(' ', '-'))
+                                .join('-')
+                            }
+                        >
+                            <Text fontSize={["10px", "10px", "16px", "16px"]} fontWeight="400" color="text.lightGrey">{ item.label } <br></br>{ item.year }</Text>
                             <Link
                                 href={item.href}
-                                flex={6}
                                 color="text.dark"
                                 _hover={{
                                     color: 'text.forestGreen',
                                     textDecoration: 'underline'
                                 }}
-                                fontSize={["30px", "30px", "30px", "80px"]}
+                                fontSize={["30px", "30px", "60px", "80px"]}
                                 fontWeight="500"
+                                width="max-content"
                             >
                                 { item.projectName }
                             </Link>
-                        </HStack>
+                        </Fragment>
                     )
                 })}
-            </>
+            </SimpleGrid>
         </Box>
     )
 }
